469321cbcdc5f0ec2bf0f5d72d6f2dc16588fa9b
milantyopity <milant1990@gmail.com>
2026-02-22 20:52:08 +0100

    fix: remove merge conflict markers from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -22,4 +10,3 @@ createRoot(document.getElementById("root")!).render(
     <Toaster />
   </StrictMode>,
 );
->>>>>>> d4894e0 (second commit)
